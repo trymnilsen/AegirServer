09094d11cb72c31d6bee62b818adde412a311510
Trym Nilsen <trym.nilsen@gmail.com>
2015-07-02 21:51:02 +0200

    Added Validation lib

---
+++ AegirValidate/HttpValidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirValidate
+{
+    public class HttpValidate
+    {
+        public static bool ValidateHTTPAddress(string address)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(address, UriKind.Absolute, out uriResult)
+                             && (uriResult.Scheme == Uri.UriSchemeHttp);
+        }
+    }
+}
