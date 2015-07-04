60d553113f212b5c08018f8a2de58633dcc40850
Trym Nilsen <trym.nilsen@gmail.com>
2015-07-04 16:42:00 +0200

    Added messages lib

---
+++ AegirMessages/Message/Messenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirMessages.Message
+{
+
+    public class Messenger
+    {
+        /// <summary>
+        /// Creates new messenger for sending and receving messages
+        /// </summary>
+        public Messenger()
+        {
+
+        }
+        public 
+    }
+}
---
+++ AegirMessages/Message/Postbox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirMessages.Message
+{
+    public class Postbox
+    {
+        private EMessengerBufferMode bufferMode;
+
+        public Postbox(EMessengerBufferMode bufferMode)
+        {
+            this.bufferMode = bufferMode;
+        }
+        public PostMessage()
+    }
+}
