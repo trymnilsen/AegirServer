23eaaadd58ad0a364b35f3776f0910faa68ab5ca
Trym Nilsen <trym.nilsen@gmail.com>
2015-07-16 20:49:42 +0200

    some controller fixes

---
+++ AegirServer/HTTP/HttpException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace AegirServer.Module
+{
+    public class HTTPException : Exception
+    {
+        public HttpStatusCode Status { get; private set; }
+        public HTTPException(HttpStatusCode status)
+        {
+            this.Status = status;
+        }
+        public HTTPException(HttpStatusCode status, string message) :  base(message)
+        {
+            this.Status = status;
+        }
+        public HTTPException(HttpStatusCode status, string message, Exception innerException) : base(message, innerException)
+        {
+            this.Status = status;
+        }
+    }
+}
