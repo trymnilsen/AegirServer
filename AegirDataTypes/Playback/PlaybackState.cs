406501a237fd7855eb1476484fe82577d395ff96
Trym Nilsen <trym.nilsen@gmail.com>
2015-09-26 12:43:22 +0200

    Add playback types and controller

---
+++ AegirDataTypes/Playback/PlaybackState.cs
@@ -0,0 +1,15 @@
+﻿using AegirDataTypes.Workspace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirDataTypes.Playback
+{
+    public class PlaybackState
+    {
+        public double PlaybackSpeed { get; set; }
+        public PlaybackMode Mode { get; set; }
+    }
+}
---
+++ AegirServer/HTTP/Controller/PlaybackController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AegirServer.HTTP.Controller
+{
+    public class PlaybackController : HTTPController
+    {
+        public override void GetAction(string[] args)
+        {
+            if (this.Request.QueryString.GetValues("query").Length == 0)
+            {
+                throw new HTTPException(System.Net.HttpStatusCode.BadRequest);
+            }
+            string searchTerm = this.Request.QueryString.GetValues("query").First();
+            using (var context = new PersistanceContext())
+            {
+                ProjectData[] data = context.Projects.Where(x => x.ProjectName.Contains(searchTerm)).ToArray();
+
+                this.SetSuccessfulContent(data, System.Net.HttpStatusCode.OK);
+            }
+            base.GetAction(args);
+        }
+    }
+}
